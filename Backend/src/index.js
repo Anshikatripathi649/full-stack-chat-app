1a591f390aa859829b0df3671dfb1d540d22c434
Anshikatripathi649 <anshikatripathi468@gmail.com>
2026-01-08 16:48:23 +0530

    some final changes in index.js file from backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,42 +1,3 @@
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import cookieParser from 'cookie-parser';
-// import cors from 'cors';
-// import path from 'path';
-
-// import authRoutes from './routes/auth.route.js';
-// import messageRoutes from './routes/message.route.js';
-// import {connectedDB} from './lib/db.js';
-
-// import { app, server } from './lib/socket.js';
-
-// dotenv.config();
-// const port = process.env.PORT;
-// const __dirname = path.resolve;
-
-// app.use(express.json({ limit: "5mb" }));
-// app.use(express.urlencoded({ limit: "5mb", extended: true }));
-// app.use(cookieParser());
-// app.use(cors({
-//     origin:'http://localhost:5173',
-//     credentials: true, // Allows the backend to receive cookies
-// }));
-
-// app.use('/api/auth', authRoutes);
-// app.use("/api/messages", messageRoutes );
-
-// if(process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//     })
-// }
-// server.listen(port, () => {
-//     console.log(`Server is listing on the port ${port}`);
-//     connectedDB();
-// });
-
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -65,10 +26,10 @@ app.use('/api/auth', authRoutes);
 app.use("/api/messages", messageRoutes );
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
     })
 }
 server.listen(port, () => {
