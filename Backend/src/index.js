028549a49c2032554111c7bce5a665c388e70324
Anshikatripathi649 <anshikatripathi468@gmail.com>
2026-02-10 13:29:06 +0530

    error solved at production level

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from 'path';
-// import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import {connectedDB} from './lib/db.js';
@@ -12,9 +12,9 @@ import { app, server } from './lib/socket.js';
 
 dotenv.config();
 const port = process.env.PORT;
-const __dirname = path.resolve();
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+// const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
@@ -28,10 +28,7 @@ app.use('/api/auth', authRoutes);
 app.use("/api/messages", messageRoutes );
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 }
 server.listen(port, () => {
     console.log(`Server is listing on the port ${port}`);
